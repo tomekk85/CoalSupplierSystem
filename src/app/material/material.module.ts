31f5396d4b60b5f3cbe387809e7122aee430cf6a
LukaszFlis <luk8426@wp.pl>
2021-04-27 20:45:23 +0200

    Angular UI

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const MaterialComponents = [
+
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }

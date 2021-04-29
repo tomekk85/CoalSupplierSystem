c602f15787f6b267c9c03b6672cdc9a694b79201
vRafski <compate97@gmail.com>
2021-04-29 19:26:23 +0200

    Adds new layout with use of angular materials

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
-const MaterialComponents = [];
+const MaterialComponents = [
+  MatSidenavModule,
+  MatButtonModule,
+  MatListModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatCardModule
+];
 
 @NgModule({
   imports: [MaterialComponents],

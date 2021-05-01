72cdd3a3104e36bd53e4d54106666d7923ffa280
LukaszFlis <luk8426@wp.pl>
2021-05-01 16:06:54 +0200

    Angular UI - ClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {MaterialModule} from './material/material.module';
+import { LayoutModule } from '@angular/cdk/layout';
+
 
 
 @NgModule({
@@ -14,8 +14,7 @@ import {MaterialModule} from './material/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MaterialModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
